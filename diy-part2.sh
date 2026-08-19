2cc0161afd288f3038e764f226fa958afd7c8713
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-08-19 19:21:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,4 +2,3 @@
 
 sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/QiuSimons/luci-app-honk package/honk
