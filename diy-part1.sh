b572b35635d61b17c8e81e434e2515402c80fbbf
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-08-23 21:49:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo 'src-git kixdns https://github.com/JohnsonRan/luci-app-kixdns.git' >> feeds.conf.default
 echo 'src-git kdae https://github.com/QiuSimons/luci-app-dae.git' >> feeds.conf.default
 echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
