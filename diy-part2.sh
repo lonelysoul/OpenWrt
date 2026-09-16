42b0df090447c850430887d8f2e2bf51ef37dc51
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-09-16 13:17:29 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,4 +2,6 @@
 
 sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/JohnsonRan/luci-app-kixdns.git package/kixdns
+git clone https://github.com/QiuSimons/luci-app-dae.git package/dae
 # git clone https://github.com/QiuSimons/luci-app-honk package/honk
