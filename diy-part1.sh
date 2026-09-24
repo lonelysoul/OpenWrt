3dbb4b499d2d78d387c223c636364cc773009bf2
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-09-25 00:05:08 +0800

    保留dae

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,4 +3,4 @@
 
 # echo 'src-git kixdns https://github.com/JohnsonRan/luci-app-kixdns.git' >> feeds.conf.default
 # echo 'src-git kdae https://github.com/QiuSimons/luci-app-dae.git' >> feeds.conf.default
-echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
+# echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/JohnsonRan/luci-app-kixdns.git package/kixdns
+# sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# git clone https://github.com/JohnsonRan/luci-app-kixdns.git package/kixdns
 git clone https://github.com/QiuSimons/luci-app-dae.git package/dae
 # git clone https://github.com/QiuSimons/luci-app-honk package/honk
